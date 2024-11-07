3788ceb2991859905e211157b4777182fd338d66
Victor Borda <victor.borda.me@gmail.com>
2024-11-07 10:40:05 -0800

    Updating helper scripts

--- run_osm_airflow.sh
+++ run_osm_airflow.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # start_airflow.sh
-source airflow_env/bin/activate
-export AIRFLOW_HOME=~/airflow
+./activate.sh
 
 # Start webserver in background
-airflow webserver --port 8092 > webserver.log 2>&1 &
+airflow webserver --port 8090 > webserver.log 2>&1 &
 
 # Start scheduler in background
 airflow scheduler > scheduler.log 2>&1 &
 
 echo "Airflow webserver and scheduler started. See logs in webserver.log and scheduler.log."
-echo "You can access the Airflow UI at http://localhost:8092"
+echo "You can access the Airflow UI at http://localhost:8090"
